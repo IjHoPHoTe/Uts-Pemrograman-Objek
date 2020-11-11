b476ae065ccbfd151ad595f2772c3b1c485e2a46
Muhammad Fahmi Aziz <72942862+IjHoPHoTe@users.noreply.github.com>
2020-11-11 15:02:24 +0700

    Add files via upload

---
+++ UtsPemrogramanObjek/src/LuasDanKelilingPersegi/LuasDanKelilingPersegi.java
@@ -0,0 +1,21 @@
+
+package LuasDanKelilingPersegi;
+
+public class LuasDanKelilingPersegi {
+
+    public static void main(String[] args) {
+        int sisi;
+        int hasilkeliling;
+        int hasilluas;
+        
+        sisi = 10;
+        
+        hasilkeliling = sisi * sisi * sisi * sisi;
+        hasilluas = sisi * sisi;
+        
+        System.out.println ("Hasil Keliling Persegi Adalah = " + hasilkeliling);
+        System.out.println ("Hasil Luas Persegi Adalah = " + hasilluas);
+        
+    }
+    
+}
